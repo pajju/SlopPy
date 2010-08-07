7a98019b48a3daf5058c0f5c2bdbd0e73ade4a05
Philip Guo <pg@cs>
2010-08-06 22:01:07 -0700

    filled out more of the boring bytecode stack-effects table

--- Python/slop.c
+++ Python/slop.c
@@ -156,19 +156,6 @@ def getse(op, arg=None):
 */
 
 
-/*
-
-    if python_version == '2.4':
-        YIELD_VALUE = 1,0
-        IMPORT_NAME = 1,1
-    elif python_version == '2.6':
-        STORE_MAP = 2,0
-    else:
-        YIELD_VALUE = 1,1
-        IMPORT_NAME = 2,1
-*/
-
-
 // ok, we're going to REALLY tediously create some global constant
 // tables that contain the effects of individual bytecodes on the stack
 
@@ -176,7 +163,8 @@ def getse(op, arg=None):
 
 // {# elts popped off of stack, # elts pushed onto stack}
 //
-// {-1, -1} value is a sentinel
+// {-1, -1} value means "invalid opcode"
+// {-999, -999} value means "variable stack effect depending on args"
 static int bytecode_stack_effects[][2] = {
   // bytecode numbers from Include/opcode.h
 
@@ -210,7 +198,7 @@ static int bytecode_stack_effects[][2] = {
   {2, 1}, //#define BINARY_TRUE_DIVIDE 27
   {2, 1}, //#define INPLACE_FLOOR_DIVIDE 28
   {2, 1}, //#define INPLACE_TRUE_DIVIDE 29
-  {1,1}, //#define SLICE_0		30
+  {1, 1}, //#define SLICE_0		30
 /* Also uses 31-33 */
   {2, 1}, //#define SLICE_1		31
   {2, 1}, //#define SLICE_2		32
@@ -226,12 +214,12 @@ static int bytecode_stack_effects[][2] = {
   {3, 0}, //#define STORE_SLICE_1	41
   {3, 0}, //#define STORE_SLICE_2	42
   {4, 0}, //#define STORE_SLICE_3	43
-// 44
-// 45
-// 46
-// 47
-// 48
-// 49
+  {-1, -1}, // 44
+  {-1, -1}, // 45
+  {-1, -1}, // 46
+  {-1, -1}, // 47
+  {-1, -1}, // 48
+  {-1, -1}, // 49
   {1, 0}, //#define DELETE_SLICE_0 50
 /* Also uses 51-53 */
   {2, 0}, //#define DELETE_SLICE_1 51
@@ -252,7 +240,7 @@ static int bytecode_stack_effects[][2] = {
   {2, 1}, //#define BINARY_OR	66
   {2, 1}, //#define INPLACE_POWER	67
   {1, 1}, //#define GET_ITER	68
-// 69
+  {-1, -1}, // 69
   {1, 0}, //#define PRINT_EXPR	70
   {1, 0}, //#define PRINT_ITEM	71
   {0, 0}, //#define PRINT_NEWLINE	72
@@ -278,64 +266,64 @@ static int bytecode_stack_effects[][2] = {
 
   {1, 0}, //#define STORE_NAME	90	/* Index in name list */
   {0, 0}, //#define DELETE_NAME	91	/* "" */
-//#define UNPACK_SEQUENCE	92	/* Number of sequence items */
+  {-999, -999}, //#define UNPACK_SEQUENCE	92	/* Number of sequence items */
 //#define FOR_ITER	93
-// 94
+  {-1, -1}, // 94
   {2, 0}, //#define STORE_ATTR	95	/* Index in name list */
   {1, 0}, //#define DELETE_ATTR	96	/* "" */
   {1, 0}, //#define STORE_GLOBAL	97	/* "" */
   {0, 0}, //#define DELETE_GLOBAL	98	/* "" */
-//#define DUP_TOPX	99	/* number of items to duplicate */
+  {-999, -999}, //#define DUP_TOPX	99	/* number of items to duplicate */
   {0, 1}, //#define LOAD_CONST	100	/* Index in const list */
   {0, 1}, //#define LOAD_NAME	101	/* Index in name list */
-//#define BUILD_TUPLE	102	/* Number of tuple items */
-//#define BUILD_LIST	103	/* Number of list items */
+  {-999, -999}, //#define BUILD_TUPLE	102	/* Number of tuple items */
+  {-999, -999}, //#define BUILD_LIST	103	/* Number of list items */
   {0, 1}, //#define BUILD_MAP	104	/* Always zero for now */
   {1, 1}, //#define LOAD_ATTR	105	/* Index in name list */
   {2, 1}, //#define COMPARE_OP	106	/* Comparison operator */
   {1, 1}, //#define IMPORT_NAME	107	/* Index in name list */
   {1, 2}, //#define IMPORT_FROM	108	/* Index in name list */
-// 109
+  {-1, -1}, // 109
 //#define JUMP_FORWARD	110	/* Number of bytes to skip */
 //#define JUMP_IF_FALSE	111	/* "" */
 //#define JUMP_IF_TRUE	112	/* "" */
 //#define JUMP_ABSOLUTE	113	/* Target byte offset from beginning of code */
-// 114
-// 115
+  {-1, -1}, // 114
+  {-1, -1}, // 115
   {0, 1}, //#define LOAD_GLOBAL	116	/* Index in name list */
-// 117
-// 118
+  {-1, -1}, // 117
+  {-1, -1}, // 118
 //#define CONTINUE_LOOP	119	/* Start of loop (absolute) */
 //#define SETUP_LOOP	120	/* Target address (relative) */
 //#define SETUP_EXCEPT	121	/* "" */
 //#define SETUP_FINALLY	122	/* "" */
-// 123
+  {-1, -1}, // 123
   {0, 1}, //#define LOAD_FAST	124	/* Local variable number */
   {1, 0}, //#define STORE_FAST	125	/* Local variable number */
   {0, 0}, //#define DELETE_FAST	126	/* Local variable number */
-// 127
-// 128
-// 129
-//#define RAISE_VARARGS	130	/* Number of raise arguments (1, 2 or 3) */
+  {-1, -1}, // 127
+  {-1, -1}, // 128
+  {-1, -1}, // 129
+  {-999, -999}, //#define RAISE_VARARGS	130	/* Number of raise arguments (1, 2 or 3) */
 
 /* CALL_FUNCTION_XXX opcodes defined below depend on this definition */
-//#define CALL_FUNCTION	131	/* #args + (#kwargs<<8) */
-//#define MAKE_FUNCTION	132	/* #defaults */
-//#define BUILD_SLICE 	133	/* Number of items */
-//#define MAKE_CLOSURE    134     /* #free vars */
+  {-999, -999}, //#define CALL_FUNCTION	131	/* #args + (#kwargs<<8) */
+  {-999, -999}, //#define MAKE_FUNCTION	132	/* #defaults */
+  {-999, -999}, //#define BUILD_SLICE 	133	/* Number of items */
+  {-999, -999}, //#define MAKE_CLOSURE    134     /* #free vars */
   {0, 1}, //#define LOAD_CLOSURE    135     /* Load free variable from closure */
   {0, 1}, //#define LOAD_DEREF      136     /* Load and dereference from closure cell */ 
   {1, 0}, //#define STORE_DEREF     137     /* Store into cell */ 
-// 138
-// 139
+  {-1, -1}, // 138
+  {-1, -1}, // 139
 
 /* The next 3 opcodes must be contiguous and satisfy
    (CALL_FUNCTION_VAR - CALL_FUNCTION) & 3 == 1  */
-//#define CALL_FUNCTION_VAR          140	/* #args + (#kwargs<<8) */
-//#define CALL_FUNCTION_KW           141	/* #args + (#kwargs<<8) */
-//#define CALL_FUNCTION_VAR_KW       142	/* #args + (#kwargs<<8) */
+  {-999, -999}, //#define CALL_FUNCTION_VAR          140	/* #args + (#kwargs<<8) */
+  {-999, -999}, //#define CALL_FUNCTION_KW           141	/* #args + (#kwargs<<8) */
+  {-999, -999}, //#define CALL_FUNCTION_VAR_KW       142	/* #args + (#kwargs<<8) */
 
 /* Support for opargs more than 16 bits long */
-//#define EXTENDED_ARG  143
+  {-1, -1}, //#define EXTENDED_ARG  143
 };
 
