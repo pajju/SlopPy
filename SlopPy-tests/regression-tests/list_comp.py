0899d33c6f8dedcec2be96aa22b1c1062fa850b5
Philip Guo <pg@cs>
2010-08-20 07:07:50 -0700

    added new test from imran

---
+++ SlopPy-tests/regression-tests/list_comp.py
@@ -0,0 +1,9 @@
+# make sure SlopPy works with list comprehensions
+
+lst = ['1', '2', 'baaad', '3', '4']
+
+xs = [int(line) for line in lst]
+assert xs == [1, 2, 3, 4]
+y = [20*x for x in xs]
+assert y == [20, 40, 60, 80]
+
